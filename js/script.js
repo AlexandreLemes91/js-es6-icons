b8b5a8d1efb6cf03c60bc5a5956d330008d70198
AlexandreLemes91 <lemesalexandre91@gmail.com>
2021-04-19 18:21:50 +0200

    end

--- js/script.js
+++ js/script.js
@@ -97,33 +97,90 @@ const icons = [
     },
 ];
 
-/* VARIABILI */
+
+/* VARIABILI *********************************************/
 const iconsContainer = document.querySelector(".icons");
+const colors = [
+    "#001858",
+    "#ffd803",
+    "#078080",
+];
+const coloredIcons = colorIcons(icons, colors);
+const filter = document.querySelector("#type");
+const types = getType(coloredIcons);
 
-/* FUNCTION CHIAMATE */
-displayIcons(icons, iconsContainer );
+/* FUNCTION CHIAMATE **************************************/
+displayIcons(coloredIcons, iconsContainer );           
+optionSelect(types, filter);
 
-const coloredIcons = colorIcons(icons, );
+filter.addEventListener("change", () =>{
+    const option = filter.value;
+    const filteredIcons = filterIcons(coloredIcons, option)
+    displayIcons(filteredIcons, iconsContainer );
+});
 
-/* FUNCTIONS */
-
-/* funciont diplay HTML */
+/* FUNCTIONS **********************************************/
+/* funciont display HTML */
 function displayIcons(icons, iconsContainer){
     let html = "";
 
     icons.forEach((icon)=>{
-        const {family, prefix, name} = icon;
+        const {family, prefix, name, color} = icon;
 
         html +=
         `<div class="icon p20">
-            <i class="${family} ${prefix}${name}"></i>
+            <i class="${family} ${prefix}${name}" style="color: ${color}"></i>
             <h3>${name}</h3>
         </div>`;
     });
 
     iconsContainer.innerHTML = html;
-}
+};
 
 /* colorare le icons */
+function colorIcons(icons, colors){
+    const types = getType(icons);
 
+    const coloredIcons = icons.map((icon) =>{
+        const index = types.indexOf(icon.type);
 
+        return {
+            ...icon,
+            color: colors[index],
+        }
+    });
+
+    return coloredIcons;
+};
+
+function getType(icons){
+    const types = [];
+
+    icons.forEach((icon)=>{
+        if( !types.includes(icon.type) ){
+            types.push(icon.type);
+        }
+    });
+
+    return types;
+}
+
+function optionSelect(types, filter){
+    let options = "";
+    types.forEach((type)=>{
+        options +=`<option value="${type}">${type}</option>`
+    });
+
+    filter.innerHTML += options;
+}
+
+function filterIcons (icons, option){
+    if( option === "all"){
+       return icons;
+    }
+    const filtered = icons.filter((icon) =>{
+        return icon.type === option;
+    });
+
+    return filtered;
+}
