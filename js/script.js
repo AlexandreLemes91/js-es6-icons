f6d1ebcf52f987568af1b57f0af2e1f9f035100b
AlexandreLemes91 <lemesalexandre91@gmail.com>
2021-04-19 17:01:21 +0200

    display icons

---
+++ js/script.js
@@ -0,0 +1,129 @@
+const icons = [
+    {
+        name: 'cat',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas',
+    },
+    {
+        name: 'crow',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas',
+    },
+    {
+        name: 'dog',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas',
+    },
+    {
+        name: 'dove',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas',
+    },
+    {
+        name: 'dragon',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas',
+    },
+    {
+        name: 'horse',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas',
+    },
+    {
+        name: 'hippo',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas',
+    },
+    {
+        name: 'fish',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas',
+    },
+    {
+        name: 'carrot',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas',
+    },
+    {
+        name: 'apple-alt',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas',
+    },
+    {
+        name: 'lemon',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas',
+    },
+    {
+        name: 'pepper-hot',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas',
+    },
+    {
+        name: 'user-astronaut',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas',
+    },
+    {
+        name: 'user-graduate',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas',
+    },
+    {
+        name: 'user-ninja',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas',
+    },
+    {
+        name: 'user-secret',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas',
+    },
+];
+
+/* VARIABILI */
+const iconsContainer = document.querySelector(".icons");
+
+/* FUNCTION CHIAMATE */
+displayIcons(icons, iconsContainer );
+
+const coloredIcons = colorIcons(icons, );
+
+/* FUNCTIONS */
+
+/* funciont diplay HTML */
+function displayIcons(icons, iconsContainer){
+    let html = "";
+
+    icons.forEach((icon)=>{
+        const {family, prefix, name} = icon;
+
+        html +=
+        `<div class="icon p20">
+            <i class="${family} ${prefix}${name}"></i>
+            <h3>${name}</h3>
+        </div>`;
+    });
+
+    iconsContainer.innerHTML = html;
+}
+
+/* colorare le icons */
+
+
